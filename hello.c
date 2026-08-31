ddd655e1a6ee1e25266fcf5ac48443c9051f9570
ezsnail <evanzhang0408@gmail.com>
2026-08-31 15:33:47 +0000

    added my name and major

--- hello.c
+++ hello.c
@@ -5,6 +5,8 @@ int main(int argc, char **argv) {
 
     printf("Riley Schmid!\n");
     printf("I am majoring in Computer Science and Robotics Engineering.\n");
+    printf("Hello, my name is Evan Zhang!\n");
+    printf("I am majoring in Computer Science.\n");
 
     return 0;
 }
